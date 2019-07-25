b9ae7abf019b9747e5a34aabfb11b1a18f95e7b6
Camilo <mac@Mac-mini-de-Mac.local>
2019-07-25 11:14:21 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Aplicación del clima - Curso Node
+
+Recuerde ejecutar ```npm install``` para las librerías
+
+### Ejemplo:
+```
+node app -d "San Jose Costa Rica"
+```
---
+++ lugar/lugar.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+
+    const encodeUlr = encodeURI(dir);
+
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeUlr}`,
+        headers: { 'X-RapidAPI-Key': '58126711a4msh27f882ecbb5f48ap1eb9e6jsn28fe10cf004b' }
+    });
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+
+        throw new Error(`No hay resultados para ${ dir }`);
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+
+}
+
+module.exports = {
+
+    getLugarLatLng
+}
